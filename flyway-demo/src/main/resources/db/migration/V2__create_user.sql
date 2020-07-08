54654e6d8248b5114aafbf444dc2135710dfab4e
zhangzhaohui <1713156380@qq.com>
2020-07-08 18:47:22 +0800

    init branch flyway-demo

---
+++ flyway-demo/src/main/resources/db/migration/V2__create_user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `flyway`.`user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
---
+++ flyway-demo/src/test/java/com/zzh1ad/flywaydemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzh1ad.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
